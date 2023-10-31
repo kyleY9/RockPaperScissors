ef5cfc01da980e2102e5b42ef12f187933fb32e3
KyleY9 <KyleY9@nycstudents.net>
2023-10-31 11:55:02 -0400

    Hiya

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class RockPaperScissors {
+public class RockPaperScissors {
+
+    private String playerInput;
+    private String compInput;
+
+    public RockPaperScissors(String playerInput) {
+        this.playerInput = playerInput;
+    }
+
+    public void setCompInput() {
+        int rand = (int) (Math.random() * 3);
+        if (rand == 2) {
+            compInput = "scissors";
+        } else if (rand == 1) {
+            compInput = "paper";
+        } else {
+            compInput = "rock";
+        }
+    }
+
+    public String getCompInput() {
+        return compInput;
+    }
+
+    public void game() {
+        if (playerInput.equals(compInput)) {
+            System.out.println("It's a tie!");
+        } else if (playerInput.equals("rock")) {
+            if (compInput.equals("scissors")) {
+                System.out.println("Rock beats scissors, you win!");
+            } else {
+                System.out.println("Rock is beaten by paper, you lose!");
+            }
+        } else if (playerInput.equals("paper")) {
+            if (compInput.equals("rock")) {
+                System.out.println("Paper beats rock, you win!");
+            } else {
+                System.out.println("Paper is beaten by scissors, you lose!");
+            }
+        } else {
+            if (compInput.equals("paper")) {
+                System.out.println("Scissor beats paper, you win!");
+            } else {
+                System.out.println("Scissors is beaten by rock, you lose!");
+            }
+        }
+    }
 }
--- src/RockPaperScissorsRunner.java
+++ src/RockPaperScissorsRunner.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class RockPaperScissorsRunner {
+import java.util.Scanner;
+public class RockPaperScissorsRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to Rock, Paper, Scissors!");
+        System.out.println("Remember: Rock beats Scissors, Paper Beats Rock, Scissor beats Paper");
+        System.out.print("Mr.Computer has decided, choose now (rock, paper, scissors, all lower): ");
+        String userInput = scan.nextLine();
+
+        RockPaperScissors obj = new RockPaperScissors(userInput);
+        obj.setCompInput();
+        System.out.println("Your choice: " + userInput);
+        System.out.println("Mr. Computer's choice: " + obj.getCompInput());
+        obj.game();
+
+
+    }
 }
